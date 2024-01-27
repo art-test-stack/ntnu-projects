38ad718b09694a3325f2d677899cdc9be7d3bbf6
Arthur Testard <testardarthur@gmail.com>
2024-01-27 18:04:50 +0100

    creation dataset + show config

--- IT3030 - Deep Learning/project_1/model.py
+++ IT3030 - Deep Learning/project_1/model.py
@@ -1,25 +1,76 @@
 import numpy as np
+from utils import GlobalConfig, LayersConfig, read_config, adapt_config
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
+def print_empty_tab_line(width=40):
+    print(f"|{'-'* width}", end='|\n')
+
+# def print_tab_line(content, width = 40):
+#     print(f"| {content}{' '*(width - len(content) - 1)}", end='|\n')
+
+def print_tab_line(row1, row2 = '', width = 40):
+    print(f"| {row1}{' '*(9 - len(row1) - 1)} | {row2}{' '*(width - len(row2) - 12)}", end='|\n')
+
+
 class MLP:
     def __init__(
             self, 
-            dim: tuple(int) = (30, 20, 40), 
-            a_f: tuple(function) = (sigmoid, sigmoid, sigmoid),
-            lr: float = .2
+            global_config: GlobalConfig,
+            layers_config: LayersConfig,
+            show_config: bool = True
         ):
-        pass
+        for k, v in global_config.__dict__.items():
+            setattr(self, k, v)
         
+        self.init_layers(layers_config)
+        if show_config: 
+            self.global_config = global_config
+            self.layers_config = layers_config
+            self.print_config()
 
     def init_weights(self, dim):
-        self.W = [ np.random.rand(dim[k]) for k in range(len(dim)) ]
-        self.b = [ np.random.rand(1) for k in range(len(dim))]
+        W = np.array([])
+        for layer in self.layers:
+            if layer.wr == 'glorot':
+                pass
+            
+            else: 
+                pass
 
+    def init_layers(self, layers_config):
+        self.input = layers_config.input
+        self.type = layers_config.type
+        self.layers = layers_config.hidden_layers
+
+    
+
+            
     def split_minibatch(self):
         pass
-    
-    def forward(self, batch, size_minibatch: float = .8):
+
+    def forward_pass(self, X_train, y_train, epoch, size_minibatch: float = .8):
         pass
-        
+    
+    def print_config(self):
+        width = 40
+        print_empty_tab_line(width)
+        print_tab_line('GLOBAL', width=width)
+        print_empty_tab_line(width)
+        for k, v in self.global_config.__dict__.items():
+            if not k in ['input', 'layers', 'type']:
+                print_tab_line(k, str(v), width)
+        print_empty_tab_line(width)
+        print_tab_line('LAYERS', width=width)
+        print_empty_tab_line(width)
+
+        print_tab_line('input', str(self.layers_config.input), width=width)
+        k = 1
+        for layer in self.layers_config.hidden_layers:
+            print_tab_line(f"layer {k}", width=width)
+            k += 1
+            for key, v in layer.items():
+                print_tab_line('', f"{key}: {str(v)}", width=width)
+        print_tab_line('type', str(self.layers_config.type), width=width)
+        print_empty_tab_line(width)
--- IT3030 - Deep Learning/project_1/utils.py
+++ IT3030 - Deep Learning/project_1/utils.py
@@ -2,6 +2,11 @@ import yaml
 from enum import Enum
 
 
+# -------------------------------------------
+#           NEURAL NETWORK PARAMETERS
+# -------------------------------------------
+
+
 class Loss(Enum):
     cross_entropy: 'cross_entropy'
     mse: 'mse'
@@ -61,3 +66,111 @@ def adapt_config(config):
     global_config = GlobalConfig(**config['GLOBAL'])
     layers_config = LayersConfig(**config['LAYERS'])
     return global_config, layers_config
+
+
+# -------------------------------------------
+#           DATASET UTILS FUNCTIONS
+# -------------------------------------------
+
+import doodler_forall as dd
+import numpy as np
+from pathlib import Path
+from datetime import datetime
+
+_doodle_image_types = dd._doodle_image_types_
+
+def create_empty_subsets(path):
+    train_set = path / 'train_set'
+    test_set = path / 'test_set'
+    val_set = path / 'val_set'
+
+    train_set.mkdir()
+    test_set.mkdir()
+    val_set.mkdir()
+
+def generate_dataset_folder_name(size, nb_classes, n):
+    address = Path('datasets')
+    name_folder = f"dataset_{str(datetime.now().date())}_{size}_{nb_classes}_{n}"
+
+    path = address / name_folder
+    if not path.exists():
+        path.mkdir()
+        # create_empty_subsets(path)
+        return name_folder
+    
+    k = 1
+    folder = name_folder + f"_v{k}"
+    path = address / folder
+    while path.exists():
+        k += 1
+        folder = name_folder + f"_v{k}"
+        path = address / folder
+    
+    path.mkdir()
+
+    # create_empty_subsets(path)
+    return folder
+
+def split_dataset(dataset, proportions = (70, 20, 10)):
+    assert sum(proportions) == 100, "Proportions do not make 100%"
+
+    train_size = int(proportions[0] * len(dataset[0]) / 100)
+    test_size = int(proportions[1] * len(dataset[0]) / 100)
+
+    inputs, targets, labels, _, _ = dataset
+    # dataset = (inputs.astype(np.float32), targets, target_labels,(rows,cols),flat)
+    X_train = inputs[:train_size]
+    Y_train = targets[:train_size]
+    labels_train = labels[:train_size]
+
+    X_test = inputs[train_size:train_size + test_size]
+    Y_test = targets[train_size:train_size + test_size]
+    labels_test = labels[train_size:train_size + test_size]
+
+    X_val = inputs[train_size + test_size:]
+    Y_val = targets[train_size + test_size:]
+    labels_val = labels[train_size + test_size:]
+    
+    return (X_train, Y_train, labels_train), (X_test, Y_test, labels_test), (X_val, Y_val, labels_val)
+
+def save_data_in_folder(address, data_subset, X, Y, labels):
+    
+    np.save(address / f"X_{data_subset}.npy", X)
+    np.save(address / f"Y_{data_subset}.npy", Y)
+    np.save(address / f"labels_{data_subset}.npy", labels)
+
+def create_dataset(proportions = (70, 20, 10), 
+                   dataset_size = 100, image_size = 30, noise = .1, relative_sizes = [0.15,0.3], 
+                #    size_range, 
+                   flattening = False, centered = True, image_types = _doodle_image_types):
+    dataset_name = generate_dataset_folder_name(dataset_size, len(image_types), image_size)
+    
+    dataset = dd.gen_doodle_cases(count=dataset_size,rows=image_size,cols=image_size,imt=image_types, hr=relative_sizes,wr=relative_sizes,
+                    nbias=noise,cent=centered, show=False,
+                    one_hots=True,auto=False, flat=flattening,bkg=0, d4=False, fog=1, fillfrac=None, fc=(1,1),
+                    gap=1,multi=False,mono=True, dbias=0.7,poly=(4,5,6))
+    
+    (X_train, Y_train, labels_train), (X_test, Y_test, labels_test), (X_val, Y_val, labels_val) = split_dataset(dataset, proportions)
+
+    save_data_in_folder(Path('datasets') / dataset_name, 'train', X_train, Y_train, labels_train)
+    save_data_in_folder(Path('datasets') / dataset_name, 'test', X_test, Y_test, labels_test)
+    save_data_in_folder(Path('datasets') / dataset_name, 'val', X_val, Y_val, labels_val)
+    
+    return dataset_name
+
+def load_subsets(address, data_subset):
+
+    X = np.load(address / f"X_{data_subset}.npy")
+    Y = np.load(address / f"Y_{data_subset}.npy")
+    labels = np.load(address / f"labels_{data_subset}.npy")
+
+    return (X, Y, labels)
+
+def load_dataset(dataset_folder):
+    address = Path('datasets') / dataset_folder
+    
+    training_set = load_subsets(address, 'train')
+    testing_set = load_subsets(address, 'test')
+    val_set = load_subsets(address, 'val')
+
+    return training_set, testing_set, val_set
